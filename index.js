bed6955ec2c5e673c565ab49548a80cc8f03cce3
Eduardo Gimenez <edufgimenez@hotmail.com>
2023-11-17 10:35:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  if(x[slideIndex -1]) {
+    x[slideIndex - 1].style.display = "block";
+  }
+
+}
+
+showDivs(slideIndex);
